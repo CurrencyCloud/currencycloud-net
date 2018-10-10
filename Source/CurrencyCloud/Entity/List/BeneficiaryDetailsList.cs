f0019dbb12619be7f392cfff9e58d8950932ea7c
Ed Addario <ed.addario@currencycloud.com>
2018-10-10 20:38:57 +0100

    Add BeneficiaryDetailsList missing properties

--- Source/CurrencyCloud/Entity/List/BeneficiaryDetailsList.cs
+++ Source/CurrencyCloud/Entity/List/BeneficiaryDetailsList.cs
@@ -13,6 +13,24 @@ namespace CurrencyCloud.Entity.List
             public string AcctNumber { get; set; }
             public string BicSwift { get; set; }
             public string BeneficiaryEntityType { get; set; }
+            public string BeneficiaryStateOrProvince { get; set; }
+            public string BeneficiaryPostcode { get; set; }
+            public string BeneficiaryAddress { get; set; }
+            public string BeneficiaryCity { get; set; }
+            public string BeneficiaryCountry { get; set; }
+            public string BeneficiaryFirstName { get; set; }
+            public string BeneficiaryLastName { get; set; }
+            public string BeneficiaryCompanyName { get; set; }
+            public string Aba { get; set; }
+            public string SortCode { get; set; }
+            public string Iban { get; set; }
+            public string BsbCode { get; set; }
+            public string InstitutionNo { get; set; }
+            public string BankCode { get; set; }
+            public string BranchCode { get; set; }
+            public string Clabe { get; set; }
+            public string Cnaps { get; set; }
+            public string Ifsc { get; set; }
         }
 
         public List<Detail> Details { get; set; }
