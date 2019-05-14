9af462cbe9ead8a19be8ef1140a76809734eb560
Ed Addario <ed.addario@currencycloud.com>
2019-05-14 10:43:23 +0100

    Update to v5.1.0

--- Source/CurrencyCloud/Client.cs
+++ Source/CurrencyCloud/Client.cs
@@ -36,7 +36,7 @@ namespace CurrencyCloud
         private HttpClient httpClient;
         private Credentials credentials;
         private string onBehalfOf;
-        private const string userAgent = "CurrencyCloudSDK/2.0 .NET/4.0.0";
+        private const string userAgent = "CurrencyCloudSDK/2.0 .NET/5.1.0";
 
         internal string Token
         {
