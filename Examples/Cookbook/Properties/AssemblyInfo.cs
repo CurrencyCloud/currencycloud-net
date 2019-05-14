2e6f682b6f6b8ff361032da703723b292993f356
Ed Addario <ed.addario@currencycloud.com>
2019-05-14 10:51:28 +0100

    Update dependencies

--- Examples/Cookbook/Properties/AssemblyInfo.cs
+++ Examples/Cookbook/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Currencycloud")]
 [assembly: AssemblyProduct("Cookbook")]
-[assembly: AssemblyCopyright("Copyright © Currencycloud 2015-2018")]
+[assembly: AssemblyCopyright("Copyright © Currencycloud 2015-2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.3.0")]
-[assembly: AssemblyFileVersion("1.3.0")]
+[assembly: AssemblyVersion("1.3.1")]
+[assembly: AssemblyFileVersion("1.3.1")]
