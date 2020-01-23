fdc625f0a9728a35156009db72f4383110319e22
jonathancouchman <jonathan.couchman@currencycloud.com>
2020-01-23 15:03:22 +0000

    Release 5.3.5

--- Source/CurrencyCloud/Client.cs
+++ Source/CurrencyCloud/Client.cs
@@ -36,7 +36,7 @@ namespace CurrencyCloud
         private HttpClient httpClient;
         private Credentials credentials;
         private string onBehalfOf;
-        private const string userAgent = "CurrencyCloudSDK/2.0 .NET/5.2.2";
+        private const string userAgent = "CurrencyCloudSDK/2.0 .NET/5.3.5";
 
         internal string Token
         {
