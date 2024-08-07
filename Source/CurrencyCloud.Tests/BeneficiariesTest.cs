fd3bd4a8f0cd586f5812ccfbdce1b88e9acb605c
Ricardo Filipe Sousa Gomes <falconetpt@users.noreply.github.com>
2024-08-07 11:56:10 +0000

    PAYB-1151: Adds cop to the SDK

--- Source/CurrencyCloud.Tests/BeneficiariesTest.cs
+++ Source/CurrencyCloud.Tests/BeneficiariesTest.cs
@@ -60,6 +60,27 @@ namespace CurrencyCloud.Tests
         }
 
         /// <summary>
+        /// Successfully verifies a account.
+        /// </summary>
+        [Test]
+        public async Task AccountVerification()
+        {
+            player.Play("AccountVerification");
+
+            BeneficiaryAccountVerification accountVerification = await client.VerifyAccountAsync(new BeneficiaryAccountVerificationRequest
+            {
+                BankCountry = "GB",
+                AccountNumber = "1234567890",
+                RoutingCodeValue1 = "123456",
+                BeneficiaryEntityType = "individual",
+                BeneficiaryFirstName = "Test",
+                BeneficiaryLastName = "User"
+            });
+
+            Assert.AreEqual("", accountVerification.Answer);
+        }
+
+        /// <summary>
         /// Successfully creates a beneficiary.
         /// </summary>
         [Test]
