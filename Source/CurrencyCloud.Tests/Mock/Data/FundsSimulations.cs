0f8755b881325602140602cec8ef8398fae2f5ee
RoyBogner <97362403+RoyBogner@users.noreply.github.com>
2026-07-07 11:00:32 +0300

    Simulate approved/rejected funds from SDK. This allows to have the operation on code using in Back-Office systems for making testing cycle easier for all departments if needed. Adding Tests of Approved and Rejected simulations.

---
+++ Source/CurrencyCloud.Tests/Mock/Data/FundsSimulations.cs
@@ -0,0 +1,30 @@
+﻿using CurrencyCloud.Entity;
+
+namespace CurrencyCloud.Tests.Mock.Data
+{
+    static class FundsSimulations
+    {
+        private static SimulateFunds CreateFundingSimulation(SimulateFundingAction action)
+        {
+            return new SimulateFunds()
+            {
+                Id = "a93260b9-5035-4e4b-8964-178ad8758ae6",
+                ReceiverAccountNumber = "13071472",
+                ReceiverRoutingCode = "03027514",
+                Amount = 15000.80m,
+                Currency = "USD",
+                Action = action,
+                SenderName = "John Doe",
+                SenderReference = "sender-ref",
+                SenderCountry = "US",
+                SenderAccountNumber = "123456789",
+                SenderRoutingCode = "200606",
+                SenderAddress = "9303 Roslyndale Ave."
+            };
+        }
+
+        public static readonly SimulateFunds ApprovedFundingSimulation = CreateFundingSimulation(SimulateFundingAction.approve);
+
+        public static readonly SimulateFunds RejectFundingSimulation = CreateFundingSimulation(SimulateFundingAction.reject);
+    }
+}
